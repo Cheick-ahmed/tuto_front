d89eb31f80b1f1f0e20651b40c13b20d6e619284
Sidibé Cheick Ahmed <chs.ahmed_pro02@outlook.com>
2020-10-20 16:04:25 +0200

    Login page

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app dark>
+    <v-container>
+      <nuxt />
+    </v-container>
+  </v-main>
+  <v-footer
+  absolute
+  app
+  >
+  <span>&copy; Ahmed/Onboarding</span>
+</v-footer>
+</v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="pt-16">
+    <v-row align="center" justify="center">
+      <v-col sm="6" lg="8" xl="4" class="mt-16">
+        <v-form
+        ref="form"
+        @submit.prevent="submit"
+        lazy-validation
+        >
+
+        <v-text-field
+        v-model="form.email"
+        label="E-mail"
+        type="email"
+        required
+        ></v-text-field>
+
+        <v-text-field
+        v-model="form.password"
+        label="Password"
+        type="password"
+        required
+        ></v-text-field>
+
+        <v-btn
+        :disabled="validation.errors"
+        type="submit"
+        color="blue lighten-1"
+        class="mt-4"
+        >
+        Log in
+      </v-btn>
+
+    </v-form>
+  </v-col>
+</v-row>
+</div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      validation : {},
+      form : {
+        email : '',
+        password : ''
+      }
+    }
+  },
+
+  methods: {
+    validate () {
+      this.$refs.form.validate()
+    },
+    submit() {
+      console.log(this.form.email)
+    }
+  },
+}
+</script>
